03b4f6c0a9c036225d8daee5111f2a4bb0e76e7e
UIT-21522029 <96015169+Napeno@users.noreply.github.com>
2024-12-25 17:15:24 +0700

    fix service sql

--- Backend/src/config/config.js
+++ Backend/src/config/config.js
@@ -5,7 +5,7 @@ module.exports= {
     username: 'root',
     password: '123',
     database: 'web_cv',
-    host: 'localhost',
+    host: '163.47.10.150',
     port:'3306',
     dialect: 'mysql'
   },
